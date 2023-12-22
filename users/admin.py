d486385a23cdded9038d8e59798229cbc58adbe4
vitaly <dudkin231@mail.ru>
2023-12-22 10:15:20 +0300

    added changes in users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from users.models import User
+
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone', 'city', 'avatar', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('city',)
+    search_fields = ('email', 'phone')
+    filter_horizontal = ('groups',)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {
+    'null': True,
+    'blank': True
+    }
+
+
+class User(AbstractUser):
+
+    username = None
+
+    email = models.EmailField(max_length=150, unique=True, verbose_name='email')
+    phone = models.CharField(max_length=50, verbose_name='Phone', **NULLABLE)
+    city = models.CharField(max_length=50,  verbose_name='City', **NULLABLE)
+    avatar = models.ImageField(upload_to='', verbose_name='Photo', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
